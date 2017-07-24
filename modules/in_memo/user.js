5e0787a3764086010f17311ce147655cf0478dcf
张宇 <zhangyu@yuanxin2015.com>
2017-07-24 10:34:06 +0800

    init

---
+++ modules/in_memo/user.js
@@ -0,0 +1,50 @@
+/**
+ * Created by yuzhang on 2017/7/23.
+ */
+
+let USER_ID_INIT = 10000
+let users = {}
+
+class User {
+  constructor (params) {
+    if (!params.name || !params.age) throw new Error('invalid params')
+    this.name = params.name
+    this.age = params.age
+    this._id = USER_ID_INIT++
+  }
+}
+
+const createUser = async function (params) {
+  const user = new User(params)
+  users[user._id] = user
+  return user
+}
+const deleteUser = async function (id) {
+  if (!id) {throw new Error('update user must provide id')}
+  if (!users[id]) {throw new Error('Invalid id')}
+  return delete users[id]
+}
+const updateUser = async function (id, params) {
+  if (!id) {throw new Error('update user must provide id')}
+  if (!users[id]) {throw new Error('Invalid id')}
+  users[id] = Object.assign({}, users[id], params)
+  return users[id]
+}
+const getUser = async function (id) {
+  if (!id) {throw new Error('Get user must provide id')}
+  const user = users[id]
+  if (user) {
+    return user
+  } else {
+    throw new Error('Invalid id')
+  }
+}
+
+module.exports = {
+  method: User,
+  users,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+}
---
+++ routes/user.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const userModule = require('../modules/in_memo/user')
+
+/* GET users listing. */
+
+router.route('/')
+  .get((req, res, next) => {
+    (async () => {
+      return userModule.users
+    })()
+      .then(data => res.json(data))
+      .catch(err => next(err))
+  })
+  .post((req, res, next) => {
+    (async (params) => {
+      return userModule.createUser(params)
+    })(req.body)
+      .then(data => res.json(data))
+      .catch(err => next(err))
+  })
+
+router.route('/:id')
+  .get((req, res, next) => {
+    (async (id) => {
+      return userModule.getUser(id)
+    })(req.params.id)
+      .then(data => res.json(data))
+      .catch(err => next(err))
+  })
+  .patch((req, res, next) => {
+    (async (params) => {
+      return userModule.updateUser(req.params.id, params)
+    })(req.body)
+      .then(data => res.json(data))
+      .catch(err => next(err))
+  })
+
+module.exports = router
