6ddc9f7c6e97d2ec6b78d20f67a9e64c6b3d15b0
张宇 <51410206@qq.com>
2017-08-11 13:53:11 +0800

    修复启动mongoose启动警告

--- services/mongoose.js
+++ services/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/cnode');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/cnode', {
+  useMongoClient: true,
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
   console.log('connected mongodb!')
-});
+})
