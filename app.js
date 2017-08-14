a86e0a0488ecb5c762744f0897b7afb62af03449
张宇 <51410206@qq.com>
2017-08-14 17:23:00 +0800

    改写app.js var写法

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-var index = require('./routes/index');
-var user = require('./routes/user');
+const index = require('./routes/index')
+const user = require('./routes/user')
 const topic = require('./routes/topic')
 require('./services/mongoose')
 
@@ -30,7 +30,7 @@ app.use('/topic', topic);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found')
   err.status = 404;
   next(err);
 });
